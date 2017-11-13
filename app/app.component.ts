a8bff36344e501b28735297dff202c2c42dbbe41
JoanShadow <joanpecamp1993@gmail.com>
2017-11-13 18:00:08 +0100

    sesion con token

--- app/app.component.ts
+++ app/app.component.ts
@@ -13,18 +13,63 @@ export class AppComponent implements OnInit {
 	public user: User;
 	public identity; // = true;
 	public token;
+	public errorMessage;
 
 	constructor(private _userService:UserService) {
 		this.user = new User('','','','','','ROLE_USER','');
 	}
 
 	ngOnInit() {
-		var texto = this._userService.signUp();
-		console.log(texto);
+
 	} 
 
 	public onSubmit() {
-		console.log(this.user);
-	}
+			console.log(this.user);
+		// Conseguir datos del usuario identificado
+		this._userService.signUp(this.user).subscribe(response => {
+			let identity = response.user;
+			this.identity = identity;
 
+			if(!this.identity._id) {
+				alert('El usuario no está correctamente identificado');
+			}else {
+				// Crear elemento en el localstorage para tener al usuario en sessión
+
+				// Conseguir el token para enviarselo a cada petición http
+
+				this._userService.signUp(this.user, 'true').subscribe(response => {
+					let token = response.user;
+					this.token = token;
+
+					if(this.token.length <= 0) {
+						alert('El token no se ha generado correctamente');
+					}else {
+						// Crear elemento en el localstorage para tener el token disponible
+
+						console.log(token);
+						console.log(identity);
+
+					}
+				},
+				error => {
+					var errorMessage = <any>error;
+
+					if(errorMessage != null) {
+						var body = JSON.parse(error._body);
+						this.errorMessage = body.message;
+						console.log(error);
+					}
+				});
+			}
+		},
+		error => {
+			var errorMessage = <any>error;
+
+			if(errorMessage != null) {
+				var body = JSON.parse(error._body);
+				this.errorMessage = body.message;
+				console.log(error);
+			}
+		});
+	}
 }
--- app/services/user.service.ts
+++ app/services/user.service.ts
@@ -13,7 +13,16 @@ export class UserService {
 		this.url = GLOBAL.url;
 	}
 
-	signUp() {
-		return 'Hola mundo desde el servicio';
+	signUp(user_to_login, gethash = null) {
+		if(gethash != null) {
+			user_to_login.gethash = gethash;
+		}
+
+		let json = JSON.stringify(user_to_login);
+		let params = json;
+
+		let headers = new Headers({'Content-Type':'application/json'});
+
+		return this._http.post(this.url+'login', params, {headers: headers}).map(res =>  res.json());
 	}
 }
