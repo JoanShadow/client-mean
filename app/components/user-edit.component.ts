8adb1fd1dd2f0c19e9d5c62dbd027032b4b1483a
JoanShadow <joanpecamp1993@gmail.com>
2017-11-27 15:12:54 +0100

    xhr

--- app/components/user-edit.component.ts
+++ app/components/user-edit.component.ts
@@ -14,6 +14,7 @@ export class UserEditComponent implements OnInit{
 	public identity;
 	public token;
 	public alertMessage;
+	public succesUpdate;
 
 	constructor(private _userService: UserService) {
 		this.titulo = 'Actualizar mis datos';
@@ -36,7 +37,7 @@ export class UserEditComponent implements OnInit{
 				//this.user = response.user;
 				localStorage.setItem("identity", JSON.stringify(this.user));
 				document.getElementById("identity_name").innerHTML = this.user.name;
-				this.alertMessage = "El usuario se ha actualizado";
+				this.succesUpdate = "El usuario se ha actualizado";
 			}
 		},
 		error => {
@@ -49,4 +50,37 @@ export class UserEditComponent implements OnInit{
 			}
 		});
 	}
+
+	public filesToUpload: Array<File>;
+
+	fileChangeEvent(fileInput:any) {
+
+		this.filesToUpload = <Array<File>>fileInput.target.files; //target.files archivos seleccionados en el input
+	}
+
+	makeFileRequest(url:string, params: Array<string>, files: Array<File>) {
+		var token = this.token;
+
+		return new Promise((resolve, reject) => {
+			var formData:any = new FormData();
+			var xhr = new XMLHttpRequest(); // Proporciona una forma fácil de obtener información de una URL sin tener que recargar la página completa
+				/*** 
+					0	UNSENT	Client has been created. open() not called yet.
+					1	OPENED	open() has been called.
+					2	HEADERS_RECEIVED	send() has been called, and headers and status are available.
+					3	LOADING	Downloading; responseText holds partial data.
+					4	DONE	The operation is complete.
+				***/
+			for(var i = 0; i < files.length; i++) {
+				formData.append('image', files[i], files[i].name);
+			}
+
+			xhr.onreadystatechange = function() {
+				if(xhr.readyState == 4) {
+					resolve(JSON.parse(xhr.response));
+				}else {
+					reject(xhr.response); 
+				}
+			}
+		});
 }
