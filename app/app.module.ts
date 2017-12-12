9dadad5191bf5e1edbb4fff973faf7201b15c2a9
JoanShadow <joanpecamp1993@gmail.com>
2017-12-12 21:00:07 +0100

    albumComponent

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,12 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { routing, appRoutingProviders } from './app.routing';
 
+// USER
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home.component';
 import { UserEditComponent } from './components/user-edit.component';
+
+// ARTIST
 import { ArtistListComponent } from './components/artist-list.component';
 import { ArtistAddComponent } from './components/artist-add.component';
 import { ArtistEditComponent } from './components/artist-edit.component';
+import { ArtistDetailComponent } from './components/artist-detail.component';
+
+// ALBUM
+import { AlbumAddComponent } from './components/album-add.component';
+import { AlbumEditComponent } from './components/album-edit.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +26,10 @@ import { ArtistEditComponent } from './components/artist-edit.component';
     UserEditComponent,
     ArtistListComponent,
     ArtistAddComponent,
-    ArtistEditComponent
+    ArtistEditComponent,
+    ArtistDetailComponent,
+    AlbumAddComponent,
+    AlbumEditComponent
   ],
   imports: [
     BrowserModule,
--- app/app.routing.ts
+++ app/app.routing.ts
@@ -1,17 +1,28 @@
 import { ModuleWithProviders} from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// USER
 import { HomeComponent } from './components/home.component';
 import { UserEditComponent } from './components/user-edit.component';
+
+// ARTIST
 import { ArtistListComponent } from './components/artist-list.component';
 import { ArtistAddComponent } from './components/artist-add.component';
 import { ArtistEditComponent } from './components/artist-edit.component';
+import { ArtistDetailComponent } from './components/artist-detail.component';
+
+// ALBUM
+import { AlbumAddComponent } from './components/album-add.component';
+import { AlbumEditComponent } from './components/album-edit.component';
 
 const appRoutes: Routes = [
 	{path: '', redirectTo: '', pathMatch: 'full', component: HomeComponent},
 	{path: 'artists/:page', component: ArtistListComponent},
 	{path: 'crear-artista', component: ArtistAddComponent},
 	{path: 'editar-artista/:id', component: ArtistEditComponent},
+	{path: 'artista/:id', component: ArtistDetailComponent},
+	{path: 'crear-album/:artist', component: AlbumAddComponent},
+	{path: 'editar-album/:id', component: AlbumEditComponent},
 	{path: 'mis-datos', component: UserEditComponent},
 	{path: '**', component: HomeComponent}
 ];
